f9c064c37a25e7d34cbfb891ad276f6402aecc24
NameOfTheLaw <tsibin.andr@gmail.com>
2016-04-02 19:57:35 +0300

    Created blank MainActivity

---
+++ What-s-cooking/app/src/main/java/com/application/vias/what_s_cooking/MainActivity.java
@@ -0,0 +1,16 @@
+package com.application.vias.what_s_cooking;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by andrey on 02.04.2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
