9929e15ccd985d4505de3f9b68023323ddb1a189
wesked <weskedstep@gmail.com>
2016-04-05 01:52:08 +0300

    Added new table's (non-stable)

--- What-s-cooking/app/src/main/java/com/application/vias/what_s_cooking/activity/DatabaseHelper.java
+++ What-s-cooking/app/src/main/java/com/application/vias/what_s_cooking/activity/DatabaseHelper.java
@@ -11,46 +11,148 @@ import android.util.Log;
 /**
  * Created by wesked on 04.04.16.
  */
-public class DatabaseHelper extends SQLiteOpenHelper implements BaseColumns{
-
-    //созданице таблицы Instruction
-    //
-    //
-    //названия столбцов
-    public static final String DISCRIPTION_COLUMN = "discription";
-    public static final String TIMER_COLUMN = "timer";
+public class DatabaseHelper extends SQLiteOpenHelper implements BaseColumns {
 
     //имя базы данных
     private static final String DATABASE_NAME = "database.db";
-
     //версия базы даннх
     private static final int DATABASE_VERSION = 1;
+    //созданице таблицы Instruction #1
+    //названия столбцов
+    public static final String INSTRUCTION_DISCRIPTION_COLUMN = "discription";
+    public static final String INSTRUCTION_TIMER_COLUMN = "timer";
+    public static final String INSTRUCTION_IMAGE_COLUMN = "image";
 
     //имя таблицы
-    private static final String DATABASE_TABLE1 = "instruction";
+    private static final String DATABASE_TABLE1 = "INSTRUCTION";
     private static final String DATABASE_CREATE_SCRIPT1 = "create table "
             + DATABASE_TABLE1 + "( " + BaseColumns._ID
-            + " integer primary key autoincrement, " + DISCRIPTION_COLUMN
-            + " text not null, " + TIMER_COLUMN + " integer);";
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, " + INSTRUCTION_DISCRIPTION_COLUMN
+            + " text not null, " + INSTRUCTION_TIMER_COLUMN + " integer, " + INSTRUCTION_IMAGE_COLUMN + " text);";
 
 
 
-    //создание таблицы Tag
+
+    //создание таблицы Tag #2
     //названия столбцов
-    public static final String NAME_COLUMN = "name";
-    public static final String LINK_COLUMN = "link";
-
-
+    public static final String TAG_NAME_COLUMN = "name";
     //имя таблицы
     private static final String DATABASE_TABLE2 = "tag";
     private static final String DATABASE_CREATE_SCRIPT2 = "create table "
             + DATABASE_TABLE2 + "( " + BaseColumns._ID
-            + " integer primary key autoincrement, " + NAME_COLUMN
-            + " text not null, " + LINK_COLUMN + " integer);";
+            + " integer primary key autoincrement, " + TAG_NAME_COLUMN
+            + " text not null);";
+
+
+
+/*
+    //Созадние таблицы Foodcategory #3
+    //названия столбцов
+    public static final String FOODCATEGORY_NAME_COLUMN = "name";
+    //имя таблицы
+    private static final String DATABASE_TABLE3 = "INSTRUCTION";
+    private static final String DATABASE_CREATE_SCRIPT3 = "create table "
+            + DATABASE_TABLE3 + "( " + BaseColumns._ID
+            + " integer primary key autoincrement, " + FOODCATEGORY_NAME_COLUMN + " TEXT NOT NULL);";
+
+
+
+
+    //Создание таблицы INGREDIENT #4
+    //названия столбцов
+    public static final String INGREDIENT_NAME_COLUMN = "name";
+    public static final String INGREDIENT_CATEGORY_COLUMN = "category";
+    public static final String INGREDIENT_FOREIGNKEY_COLUMN = "foreign key";
+    //имя таблицы
+    private static final String DATABASE_TABLE4 = "INGREDIENT";
+    private static final String DATABASE_CREATE_SCRIPT4 = "create table "
+            + DATABASE_TABLE4 + "( " + BaseColumns._ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, " + INGREDIENT_NAME_COLUMN + " TEXT NOT NULL, "
+            + INGREDIENT_CATEGORY_COLUMN + " INTEGER, "
+            + INGREDIENT_FOREIGNKEY_COLUMN + " (CATEGORY) REFERENCES FOODCATEGORY(_id));";
+
+
+
+
+    //Созадние таблицы Dish #5
+    //названия столбцов
+    public static final String DISH_NAME_COLUMN = "name";
+    public static final String DISH_RSIMPLE_COLUMN = "r_simple";
+    public static final String DISH_RORIGIN_COLUMN = "r_origin";
+    public static final String DISH_RCASHTIME_COLUMN = "r_cashtime";
+    public static final String DISH_DATACREATE_COLUMN = "data_create";
+    //имя таблицы
+    private static final String DATABASE_TABLE5 = "DISH";
+    private static final String DATABASE_CREATE_SCRIPT5 = "create table "
+            + DATABASE_TABLE5 + "( " + BaseColumns._ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, " + DISH_NAME_COLUMN + " TEXT NOT NULL, "
+            + DISH_RSIMPLE_COLUMN + " REAL, "
+            + DISH_RORIGIN_COLUMN + " REAL, "
+            + DISH_RCASHTIME_COLUMN + " REAL,"
+            + DISH_DATACREATE_COLUMN + " TEXT);";
+
+
+
+
+    //Созадние таблицы Dish_INGR #6
+    //названия столбцов
+    public static final String DISHINGR_DISH_COLUMN = "dish";
+    public static final String DISHINGR_INSTRUCTION_COLUMN = "ingredient";
+    public static final String DISHINGR_FOREIGNKEYDISH_COLUMN = "foreign key";
+    public static final String DISHINGR_FOREIGNKEYINSTRUCTION_COLUMN = "foreign key";
+
+    //имя таблицы
+    private static final String DATABASE_TABLE6 = "DISH_INGR";
+    private static final String DATABASE_CREATE_SCRIPT6 = "create table "
+            + DATABASE_TABLE6 + "( " + BaseColumns._ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + DISHINGR_DISH_COLUMN + " INGEGER, " + DISHINGR_INSTRUCTION_COLUMN + " INTEGER, "
+            + DISHINGR_FOREIGNKEYDISH_COLUMN + " (dish) REFERENCES DISH(_id), "
+            + DISHINGR_FOREIGNKEYINSTRUCTION_COLUMN + " (ingredient) REFERENCES INGREDIENT(_id));";
+
+
+
+    //Созадние таблицы Dish_INSTR #7
+    //названия столбцов
+    public static final String DISHINSTR_DISH_COLUMN = "dish";
+    public static final String DISHINSTR_INSTRUCTION_COLUMN = "instruction";
+    public static final String DISHINSTR_FOREIGNKEYDISH_COLUMN = "foreign key";
+    public static final String DISHINSTR_FOREIGNKEYINSTRUCTION_COLUMN = "foreign key";
+
+    //имя таблицы
+    private static final String DATABASE_TABLE7 = "DISH_INSTR";
+    private static final String DATABASE_CREATE_SCRIPT7 = "create table "
+            + DATABASE_TABLE7 + "( " + BaseColumns._ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + DISHINSTR_DISH_COLUMN + " INGEGER, " + DISHINSTR_INSTRUCTION_COLUMN + " INTEGER, "
+            + DISHINSTR_FOREIGNKEYDISH_COLUMN + " (dish) REFERENCES DISH(_id), "
+            + DISHINSTR_FOREIGNKEYINSTRUCTION_COLUMN + " (instruction) REFERENCES INGREDIENT(_id));";
+
+
+
+    //Созадние таблицы Dish_TAG #8
+    //названия столбцов
+    public static final String DISHTAG_DISH_COLUMN = "dish";
+    public static final String DISHTAG_TAG_COLUMN = "tag";
+    public static final String DISHTAG_FOREIGNKEYDISH_COLUMN = "foreign key";
+    public static final String DISHTAG_FOREIGNKEYTAG_COLUMN = "foreign key";
+
+    //имя таблицы
+    private static final String DATABASE_TABLE8 = "DISH_TAG";
+    private static final String DATABASE_CREATE_SCRIPT8 = "create table "
+            + DATABASE_TABLE8 + "( " + BaseColumns._ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + DISHTAG_DISH_COLUMN + " INGEGER, " + DISHTAG_TAG_COLUMN + " INTEGER, "
+            + DISHTAG_FOREIGNKEYDISH_COLUMN + " (dish) REFERENCES DISH(_id), "
+            + DISHTAG_FOREIGNKEYTAG_COLUMN + " (instruction) REFERENCES TAG(_id));";
+
+
+
+*/
+
 
     public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
-
     }
 
     //тут из MainActivity посылается сигнал на создании БД
@@ -59,6 +161,13 @@ public class DatabaseHelper extends SQLiteOpenHelper implements BaseColumns{
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(DATABASE_CREATE_SCRIPT1);
         db.execSQL(DATABASE_CREATE_SCRIPT2);
+  /*    db.execSQL(DATABASE_CREATE_SCRIPT3);
+        db.execSQL(DATABASE_CREATE_SCRIPT4);
+        db.execSQL(DATABASE_CREATE_SCRIPT5);
+        db.execSQL(DATABASE_CREATE_SCRIPT6);
+        db.execSQL(DATABASE_CREATE_SCRIPT7);
+        db.execSQL(DATABASE_CREATE_SCRIPT8);*/
+
 
     }
 
