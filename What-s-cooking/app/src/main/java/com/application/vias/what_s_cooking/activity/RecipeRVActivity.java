bc1a5c0b7be8dfc1655272e61cf5f9a7472c2712
wesked <weskedstep@gmail.com>
2016-05-19 03:49:08 +0300

    small changes RecipeRVActivity, replace "recepts" on "recipe", small changes on RV

---
+++ What-s-cooking/app/src/main/java/com/application/vias/what_s_cooking/activity/RecipeRVActivity.java
@@ -0,0 +1,55 @@
+package com.application.vias.what_s_cooking.activity;
+
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import com.application.vias.what_s_cooking.ApplicationState;
+import com.application.vias.what_s_cooking.DatabaseHelper;
+import com.application.vias.what_s_cooking.R;
+import com.application.vias.what_s_cooking.adapter.RecipeAdapter;
+import com.application.vias.what_s_cooking.entity.Dish;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecipeRVActivity extends AbstractActivity {
+
+    public List<Dish> recipe;
+    public RecyclerView recyclerView;
+    {}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.recipe_rv_activity);
+
+        initializeData();
+        initializeAdapter();
+
+
+
+    }
+
+    private void initializeData() {
+        recipe = new ArrayList<>();
+        ApplicationState state = ApplicationState.getInstance();
+        DatabaseHelper helper = state.getHelper();
+
+        List<Dish> dishList = helper.getDishesByIngredients(helper.getAllIngredients());
+        //List<Dish> tagList = helper.getTagByIngredients()
+        recipe.addAll(dishList);
+
+    }
+
+    public void initializeAdapter() {
+        recyclerView = (RecyclerView) findViewById(R.id.recipe_rv);
+        LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(llm);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        RecipeAdapter adapter = new RecipeAdapter(recipe);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+}
---
+++ What-s-cooking/app/src/main/java/com/application/vias/what_s_cooking/adapter/RecipeAdapter.java
@@ -0,0 +1,74 @@
+package com.application.vias.what_s_cooking.adapter;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.text.Spanned;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.application.vias.what_s_cooking.R;
+import com.application.vias.what_s_cooking.entity.Dish;
+
+import java.util.List;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.recipeViewHolder> {
+
+    public static class recipeViewHolder extends RecyclerView.ViewHolder {
+
+        CardView cv;
+        TextView recipeName;
+        TextView recipeText;
+        ImageView recipePhoto;
+        ListView recipeTag;
+
+        recipeViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView)itemView.findViewById(R.id.cv);
+            recipeName = (TextView)itemView.findViewById(R.id.recipe_name);
+            recipeText = (TextView)itemView.findViewById(R.id.recipe_text);
+            recipePhoto = (ImageView)itemView.findViewById(R.id.recipe_photo);
+
+
+    }
+}
+
+    List<Dish> recipes;
+
+    public RecipeAdapter(List<Dish> recipes){
+        this.recipes = recipes;
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    @Override
+    public recipeViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recipe_item, viewGroup, false);
+        recipeViewHolder pvh = new recipeViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(recipeViewHolder recipeViewHolder, int i) {
+        final Dish recipe = recipes.get(i);
+        recipeViewHolder.recipeName.setText(recipe.getName());
+        recipeViewHolder.recipeText.setText((CharSequence) "Описание: " + "\n" + "Тут будет описание, но его пока нет. Т.к его не в базе. Но будет выглядеть примерно так");
+        //recipeViewHolder.recipeTag
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipes.size();
+    }
+
+
+}
+
+
